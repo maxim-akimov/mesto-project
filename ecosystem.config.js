53989dce26c979880d3d4b50855596fee2f0c0ad
Maksim Akimov <79629719220@ya.ru>
2024-03-22 15:47:56 +0300

    Конфигурация для деплоя

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/main',
+} = process.env;
+
+module.exports = {
+  apps : [{
+    name   : "mesto-frontend",
+    script : "./dist/index.html"
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/maxim-akimov/mesto-project.git',
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env.production ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+}
